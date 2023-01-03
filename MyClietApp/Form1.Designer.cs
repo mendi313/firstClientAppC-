2ec91e5d2473d0ab0c924b39bcb14ee7da518326
mendi weissman <770mmw@gmail.com>
2023-01-03 16:02:09 +0200

    Add project files.

---
+++ MyClietApp/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace MyClietApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(95, 88);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(716, 374);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.textBoxID.Location = new System.Drawing.Point(95, 23);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(314, 32);
+            this.textBoxID.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(635, 23);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(175, 48);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 494);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ MyClietApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net.Http.Formatting;
+using System.Net.Http;
+
+namespace MyClietApp
+{
+    public partial class Form1 : Form
+    {
+        localhost.WebService1 proxy = new localhost.WebService1();
+        HttpClient client = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void WebServicesSettings()
+        {
+            client.BaseAddress = new Uri("https://localhost:44332/WebService1.asmx/");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string countriesJson =  proxy.Countries();
+            // dataGridView1.DataSource = JsonConvert.DeserializeObject<DataTable>(countriesJson);
+            WebServicesSettings();
+
+        }
+        private DataTable stringSplit(string userJson)
+        {
+            string[] json = userJson.Split('>');
+            string[] finalJson = json[2].Split('<');
+            DataTable dt = JsonConvert.DeserializeObject<DataTable>(finalJson[0]);
+            return dt;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            HttpResponseMessage message = client.GetAsync("dataTableForUsers?id=" +textBoxID.Text + "").Result;
+            string userJson = message.Content.ReadAsStringAsync().Result;
+            MessageBox.Show(userJson);
+            dataGridView1.DataSource = stringSplit(userJson);
+        }
+    }
+}
